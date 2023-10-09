1b5c7ddfb45ece56696c0e9828e0e921fdb0fc6d
SirBlaz333 <arseniyserebryakov17@gmail.com>
2023-10-09 15:19:33 +0200

    add som functions to NelistParser to work with regular expression

--- src/netlistparser.cpp
+++ src/netlistparser.cpp
@@ -1,13 +1,29 @@
 #include "headers/netlistparser.h"
 
 #include <headers/circuit.h>
+#include <regex>
 
-NetlistParser::NetlistParser()
-{
+NetlistParser::NetlistParser() {}
 
+Circuit NetlistParser::parseLibreNotation(std::string input){
 }
 
-Circuit NetlistParser::parseLibreNotation(std::string input)
+bool hasMatches(std::string input, std::regex regex)
 {
+    return std::regex_search(input, regex);
+}
 
+bool isMatch(std::string input, std::regex regex)
+{
+    return std::regex_match(input, regex);
+}
+
+std::vector<std::string> findAllMatches(std::string input, std::regex regex)
+{
+    std::vector<std::string> vector;
+    std::smatch match;
+    while (std::regex_search(input, match, regex)) {
+        vector.push_back(match.suffix().str());
+    }
+    return vector;
 }
