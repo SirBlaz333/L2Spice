a5f25f7e2ea3c5ab4b3276a5dc0d3f4921128a43
SirBlaz333 <arseniyserebryakov17@gmail.com>
2023-10-24 15:43:27 +0200

    Add comments to the code

--- src/netlistparser.cpp
+++ src/netlistparser.cpp
@@ -18,19 +18,19 @@ std::string NetlistParser::nextWord()
 std::string NetlistParser::nextDataInQuotes()
 {
     std::string result;
-    currentCharacter++;
+    currentCharacter++; //skip the opening quote.
     while (!character_utils::isQuotes(*currentCharacter)) {
         result += *currentCharacter;
         currentCharacter++;
     }
-    currentCharacter++;
+    currentCharacter++; //skip the closing quote.
     return result;
 }
 
 void NetlistParser::parseComponents(std::string::iterator last)
 {
     while (currentCharacter != last) {
-        parseComponent("", last);
+        parseComponent("", last); // parsing elements in netlist.
     }
 }
 
@@ -40,10 +40,10 @@ void NetlistParser::parseComponent(std::string parentUuid,
     while (currentCharacter != last) {
         currentCharacter++;
         if (character_utils::isLeftParanthesis(*currentCharacter)) {
-            parseElement(parentUuid, last);
+            parseElement(parentUuid, last); // parse element in ();
         }
         if (character_utils::isRightParanthesis(*currentCharacter)) {
-            return;
+            return; // return, because it is the end of the element;
         }
     }
 }
@@ -52,18 +52,26 @@ void NetlistParser::parseElement(std::string parentUuid,
                                  std::string::iterator last)
 {
     currentCharacter++;
-    std::string name = nextWord();
+    std::string name = nextWord(); // get the name of element/property/nested element.
     currentCharacter++;
-    std::string value = nextWord();
+    std::string value = nextWord(); // get either uuid/property or attribute name.
+    // if it is an element, then there is gonna be a whitespace character afterwards.
     if (character_utils::isWhitespaceCharacter(*currentCharacter)) {
+        // create new element based on its name and uuid (name for attribute).
         Element *element = createNewElement(name, value);
-        parseComponent(element->getUuid(), last);
+        parseComponent(element->getUuid(), last); //parse the nested properties or elements.
+        // if the element has an parent element, we need to add it to the parent as a property.
+        // only the highest elements in hierarchy don't have parents.
         if (!parentUuid.empty()) {
             Element *parent = elementMap[parentUuid].get();
             parent->setProperty(name, element);
         }
-    } else if (character_utils::isRightParanthesis(*currentCharacter)) {
+    }
+    // if it as property, then there is gonna be ')' afterwards.
+    else if (character_utils::isRightParanthesis(*currentCharacter)) {
+        // get the parent element from the storage using uuid
         Element *parent = elementMap[parentUuid].get();
+        //if an element with current uuid is already in the storage, just add it to the parent.
         if (elementMap.count(value) != 0) {
             Element *element = elementMap[value].get();
             parent->setProperty(name, element);
@@ -76,7 +84,9 @@ void NetlistParser::parseElement(std::string parentUuid,
 
 Element* NetlistParser::createNewElement(std::string name, std::string uuid)
 {
+    // create pointer to Element's child from the factory.
     std::unique_ptr<Element> element = elementFactory[name]();
+    // if it is an attribute, we need to create uuid ourselves and set the name to the attribute
     if (element->getType() == "attribute") {
         std::string name = uuid;
         uuid = uuid_generator::generateUUID();
@@ -105,10 +115,13 @@ Circuit NetlistParser::parseLibreNotation(std::string input)
     currentCharacter = input.begin();
     char front = *currentCharacter;
     currentCharacter++;
+    // check if the first character is '(' and the format is "librepcb_circuit".
+    // TODO: if it is not right now or something will went wrong later, we need to show some message to the user.
     if (character_utils::isLeftParanthesis(front) && nextWord() == "librepcb_circuit") {
         parseComponents(input.end());
     }
 
+    // create the circuit from the elements.
     Circuit circuit;
     auto iterator = elementMap.begin();
     while (iterator != elementMap.end()) {
