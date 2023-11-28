9b4bd717361afcdfd216ae82846e30f315be3ad3
SirBlaz333 <arseniyserebryakov17@gmail.com>
2023-11-28 14:28:48 +0100

    Move styles initialization to another function

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,24 @@
 #include <QFile>
 #include <QSettings>
 
+QString initStyles();
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QSettings settings("USMB", "NetlistConverter");
-    QFile stylesheet(":styles/stylesheet.qss");
-    if (stylesheet.open(QIODevice::ReadOnly | QIODevice::Text)) {
-        QString styles = stylesheet.readAll();
-        a.setStyleSheet(styles);
-        stylesheet.close();
-    }
+    a.setStyleSheet(initStyles());
     MainWindow w;
     w.show();
     return a.exec();
+}
+
+QString initStyles()
+{
+    QFile stylesheet(":styles/stylesheet.qss");
+    if (stylesheet.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        QString styles = stylesheet.readAll();
+        stylesheet.close();
+        return styles;
+    }
+    return "";
 }
